1b8826faee6a462a787a9474511365ce93efc04d
Yann Guillerm <yann.gv29@gmail.com>
2021-05-15 14:21:28 +0200

    add initDictionary function to use another dictionary file

--- README.md
+++ README.md
@@ -4,4 +4,16 @@ Boggle dictionary wrapper
 
 Provides quick lookup of words based on Boggle/Scrabble dictionary
 
+# usage
+
+you must init dictionary by calling
+
+  var dic = require('prefix-dictionary');
+  dic.initDictionary();
+
+you can also use your own dictionary file ( french for example)
+
+  var dic = require('prefix-dictionary');
+  dic.initDictionary(path.join( _ _ dirname, 'osd8.txt'););
+
 ### lower-case words only!
--- index.js
+++ index.js
@@ -13,13 +13,19 @@ function initDictionary(filename) {
   return d;
 }
 
-var start = new Date();
-var dictionary = initDictionary(path.join(__dirname, 'ospd4.txt'));
-var finish = new Date();
-console.log('dictionary init took', finish - start, 'ms');
-// console.log(dictionary)
+
 
 module.exports = {
+  initDictionary: function (fileFullName) {
+    if ( fileFullName == undefined ) {
+      fileFullName = path.join(__dirname, 'ospd4.txt');
+    }
+    var start = new Date();
+    var dictionary = initDictionary(fileFullName);
+    var finish = new Date();
+    console.log('dictionary init took', finish - start, 'ms');
+    // console.log(dictionary)
+  }
   isWord: function (word) {
     return dictionary.lookup(word);
   },
